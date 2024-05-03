efaa91c35cf1b150696121eba8d479568b0e3a02
Daniel Shi <danielshi@Daniels-MBP.lan>
2024-05-03 00:30:17 -0700

    Updating favicon

---
+++ app/components/meta.js
@@ -0,0 +1,38 @@
+import Head from 'next/head'
+import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
+// import test from "../../public/favicon.ico"
+
+export default function Meta() {
+  return (
+    <Head>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/../../public/favicon.ico"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/../../public/favicon.ico"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/../../public/favicon.ico"
+      />
+      {/* <link rel="manifest" href="/favicon/site.webmanifest" /> */}
+      <link rel="shortcut icon" href="/favicon/favicon-32x32.png" />
+      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="msapplication-config" content="/favicon/favicon-32x32.png" />
+      <meta name="theme-color" content="#000" />
+      {/* <link rel="alternate" type="application/rss+xml" href="/feed.xml" /> */}
+      <meta
+        name="description"
+        content={`Daniel Shi Portfolio`}
+      />
+      {/* <meta property="og:image" content={HOME_OG_IMAGE_URL} /> */}
+    </Head>
+  )
+}
