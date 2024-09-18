3e2c240a39d0f11e13c41f81aea90cec584f1fa1
littlepisces <jacky@littlepisces.com>
2024-09-18 01:11:57 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Landing from "./components/landing";
 import StoreInfo from "./components/StoreInfo";
-import Carousel from "./components/Carousel";
 
 export default function Page() {
   return (
@@ -8,9 +7,6 @@ export default function Page() {
       <Landing />
       <StoreInfo />
       <div className="bg-orange">
-        <div className="max-w-[1300px] m-auto">
-          <Carousel />
-        </div>
       </div>
     </div>
   );
