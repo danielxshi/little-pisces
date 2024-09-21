5f0ac59e29e3e0bf1a47bc9cd9ace77777052522
littlepisces <jacky@littlepisces.com>
2024-09-20 23:49:37 -0700

    Update page.tsx

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -63,7 +63,7 @@ export default function Menu() {
           <ul className="w-full">
             {ProjectMessages.MenuSoftServe.map((item, index) => (
               <li className="w-full flex  mb-[-2.5rem] ">
-                <div className="w-28  h-28 md:ml-[-2rem] min-w-[100px] max-w-[100px] min-h-[100px] max-h-[120px]">
+                <div className="w-18 h-18 md:ml-[-2rem] min-w-[100px] max-w-[100px] min-h-[100px] max-h-[120px]">
                   <ContentfulImage
                     src={`/images/menu-icons/${item.image}`}
                     fill={true}
