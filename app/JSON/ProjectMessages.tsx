3f22ce010e2be0543222af0b6bf6b4e6f986ffa6
littlepisces <jacky@littlepisces.com>
2024-10-08 19:40:48 -0700

    Update ProjectMessages.tsx

--- app/JSON/ProjectMessages.tsx
+++ app/JSON/ProjectMessages.tsx
@@ -70,14 +70,14 @@ const MenuTaiyakiSoftServe = [
 ];
 
 const MenuDrinks = [
-  { name: "Hojicha Tea ❄️🔥", price: "4.0", image: "hojicha tea.svg" },
-  { name: "Passionfruit Green Tea ❄️", price: "5.0", image: "passion fruit green tea drink.svg" },
-  { name: "Taro Milk ❄️🔥", price: "6.5", image: "taro milk.svg" },
-  { name: "Matcha Latte ❄️🔥", price: "6.5", image: "matcha latte.svg" },
+  { name: "Hojicha Tea", price: "4.0", image: "hojicha tea.svg" },
+  { name: "Passionfruit Green Tea", price: "5.0", image: "passion fruit green tea drink.svg" },
+  { name: "Taro Milk", price: "6.5", image: "taro milk.svg" },
+  { name: "Matcha Latte", price: "6.5", image: "matcha latte.svg" },
   { name: "Matcha Strawberry Latte ❄️", price: "7.5", image: "matcha strawberry latte.svg" },
-  { name: "Hojicha Latte ❄️🔥", price: "6.5", image: "hojicha latte.svg" },
-  { name: "Hojicha Taro Latte ❄️🔥", price: "7.5", image: "hojicha taro latte.svg" },
-  { name: "Milo Hot Chocolate 🔥", price: "6.0", image: "cold brew.svg" },
+  { name: "Hojicha Latte", price: "6.5", image: "hojicha latte.svg" },
+  { name: "Hojicha Taro Latte", price: "7.5", image: "hojicha taro latte.svg" },
+  { name: "Milo Hot Chocolate", price: "6.0", image: "cold brew.svg" },
 ];
 
 const ProjectMessages = {
